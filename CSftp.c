31a769f5653a881fb168d9a9c5ea0e33e7310529
Wen Xing Pan <w.pan@alumni.ubc.ca>
2018-04-06 14:08:05 -0700

    Pasv mode done

--- CSftp.c
+++ CSftp.c
@@ -2,6 +2,7 @@
 #include <sys/socket.h>
 #include <stdio.h>
 #include <string.h>
+#include <strings.h>
 #include <stdlib.h>
 #include <netdb.h>
 #include <unistd.h>
@@ -9,14 +10,62 @@
 #include "dir.h"
 #include "usage.h"
 
+#ifndef MAXHOSTNAMELEN
+#define MAXHOSTNAMELEN 256
+#endif
+
 #define BACKLOG 10
+#define BUFFSIZE 1000
+#define sendMsg(socket, msg) send(socket, msg, strlen(msg), 0)
+#define recvMsg(socket, msg) recv(socket, msg, BUFFSIZE, 0)
 
 char *portNumber;
+typedef enum command { INVALID = -1,
+                       USER,
+                       TYPE,
+                       MODE,
+                       STRU,
+                       PASV,
+                       NLST,
+                       RETR,
+                       CWD,
+                       CDUP,
+                       QUIT } command_t;
+struct
+{
+  unsigned int is_logged_in : 1;
+  unsigned int is_ascii : 1;
+  unsigned int is_fs : 1;
+  unsigned int is_passive : 1;
+  unsigned int is_stream : 1;
+} flags;
+struct addrinfo *p;
 
 void *get_in_addr(struct sockaddr *);
+void handleConnection(int *sock);
+command_t handeleCommand(char *);
+void splitStr(char *, char, char *[]);
 
 // Here is an example of how to use the above function. It also shows
 // one how to get the arguments passed on the command line.
+
+void debug(void)
+{
+  printf("ai_family %d\n", p->ai_family);
+  printf("ai_socktype %d\n", p->ai_socktype);
+  printf("ai_protocol %d\n", p->ai_protocol);
+  char ipstring[INET6_ADDRSTRLEN];
+  if (p->ai_family == AF_INET)
+  {
+
+    struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
+    void *addr = &(ipv4->sin_addr);
+    int port = htons(ipv4->sin_port);
+    printf("port is: %d\n", port);
+    inet_ntop(p->ai_family, addr, ipstring, sizeof ipstring);
+    printf(" addr: %s\n", ipstring);
+  }
+}
 
 int main(int argc, char **argv)
 {
@@ -36,7 +85,7 @@ int main(int argc, char **argv)
   int sockfd, new_fd;
   socklen_t sin_size;
 
-  struct addrinfo hints, *servinfo, *p;
+  struct addrinfo hints, *servinfo;
   struct sockaddr_storage their_addr;
 
   int yes = 1;
@@ -58,6 +107,8 @@ int main(int argc, char **argv)
 
   for (p = servinfo; p != NULL; p = p->ai_next)
   {
+    debug();
+
     if ((sockfd = socket(p->ai_family, p->ai_socktype,
                          p->ai_protocol)) == -1)
     {
@@ -82,7 +133,7 @@ int main(int argc, char **argv)
     break;
   }
 
-  freeaddrinfo(servinfo); // all done with this structure
+  //freeaddrinfo(servinfo); // all done with this structure
 
   if (p == NULL)
   {
@@ -113,8 +164,10 @@ int main(int argc, char **argv)
               s, sizeof s);
     printf("server: got connection from %s\n", s);
 
-    if (send(new_fd, "Hello, world!\n", 14, 0) == -1)
+    if (sendMsg(new_fd, "Hello, world!\n") == -1)
       perror("send");
+
+    handleConnection(new_fd);
     close(new_fd);
     exit(0);
   }
@@ -137,3 +190,280 @@ void *get_in_addr(struct sockaddr *sa)
 
   return &(((struct sockaddr_in6 *)sa)->sin6_addr);
 }
+
+void handleConnection(int *sock)
+{
+  char *msg, clientMsg[BUFFSIZE], arg[BUFFSIZE], hostName[MAXHOSTNAMELEN];
+  int recvStatus, sendStatus;
+  int pasvPort, pasvSock;
+  struct sockaddr_in pasvServer, controlServer;
+  socklen_t sin_size;
+  char ipstr[INET6_ADDRSTRLEN];
+
+  msg = "220 - Welcome to FTP server. This server only supports the username: cs317\n";
+
+  // Initialize flags
+  flags.is_logged_in = flags.is_ascii = flags.is_stream = flags.is_fs = flags.is_passive = 0;
+
+  if (sendMsg(sock, msg) == -1)
+  {
+    perror("send");
+    return;
+  }
+
+  while ((recvStatus = recvMsg(sock, clientMsg)) > 0)
+  {
+    sscanf(clientMsg, "%s", arg);
+
+    switch (handeleCommand(arg))
+    {
+    case USER:
+      if (flags.is_logged_in == 0)
+      {
+        sscanf(clientMsg, "%s %s", arg, arg);
+
+        if (!strcasecmp(arg, "cs317"))
+        {
+          flags.is_logged_in = 1;
+
+          msg = "230 - Login successful\n";
+          sendMsg(sock, msg);
+        }
+        else
+        {
+          msg = "430 - This server only supports the username: cs317\n";
+          sendMsg(sock, msg);
+        }
+      }
+      else
+      {
+        msg = "430 - Can't change from cs317\n";
+        sendMsg(sock, msg);
+      }
+
+      printf("%d\n", USER);
+      break;
+    case TYPE:
+      if (flags.is_logged_in == 1)
+      {
+        sscanf(clientMsg, "%s %s", arg, arg);
+        if (strcasecmp(arg, "A") == 0)
+        {
+          flags.is_ascii = 1;
+          msg = "200 - Switching to ASCII mode\n";
+        }
+        else if (strcasecmp(arg, "I") == 0)
+        {
+          flags.is_ascii = 0;
+          msg = "200 - Switching to Image mode\n";
+        }
+        else
+        {
+          msg = "504 - This server onlys support Type A and Type I. \n";
+        }
+      }
+      else
+      {
+        msg = "504 - Login first\n";
+      }
+      sendMsg(sock, msg);
+      printf("%d\n", TYPE);
+      break;
+    case MODE:
+      if (flags.is_logged_in == 1)
+      {
+        sscanf(clientMsg, "%s %s", arg, arg);
+
+        if (strcasecmp(arg, "S"))
+        {
+          flags.is_stream = 1;
+          msg = "200 Mode set to S.\n";
+        }
+        else
+        {
+          msg = "504 Bad MODE command.\n";
+        }
+      }
+      else
+      {
+        msg = "530 Login first.\n";
+      }
+      sendMsg(sock, msg);
+      printf("%d\n", MODE);
+      break;
+    case STRU:
+      if (flags.is_logged_in == 1)
+      {
+        sscanf(clientMsg, "%s %s", arg, arg);
+
+        if (strcasecmp(arg, "F"))
+        {
+          flags.is_fs = 1;
+          msg = "200 Data Structure set to File Structure.\n";
+        }
+        else
+        {
+          msg = "504 Bad STRU command.\n";
+        }
+      }
+      else
+      {
+        msg = "530 Login first.\n";
+      }
+      sendMsg(sock, msg);
+
+      printf("%d\n", STRU);
+      break;
+    case PASV:
+      if (flags.is_logged_in == 1)
+      {
+        // Random Port
+        do
+        {
+          pasvPort = (rand() % 64512 + 1024);
+          // New Socket
+          if ((pasvSock = socket(p->ai_family, p->ai_socktype,
+                                 p->ai_protocol)) == -1)
+          {
+            perror("server: socket");
+            continue;
+          }
+
+          pasvServer.sin_family = p->ai_family;
+          pasvServer.sin_addr.s_addr = INADDR_ANY;
+          pasvServer.sin_port = htons(pasvPort);
+
+        } while (bind(pasvSock, (struct sockaddr *)&pasvServer, sizeof(pasvServer)) < 0);
+
+        if(!(pasvPort > 1024 && pasvPort <= 65535)){
+          perror("server: pasvPort error");
+          continue;
+        }
+
+        listen(pasvSock, 1);
+        flags.is_passive = 1;
+
+        sin_size = sizeof controlServer;
+        getsockname(sock, (struct sockaddr *)&controlServer, &sin_size);
+
+        inet_ntop(p->ai_family, &controlServer.sin_addr, ipstr, sizeof ipstr);
+        printf(" addr: %s\n", ipstr);
+        char *splitIp[4];
+        splitStr(ipstr, '.', splitIp);
+
+        char temp[BUFFSIZE];
+        sprintf(temp, "227 Entering Passive Mode (%s,%s,%s,%s,%d,%d).", splitIp[0], splitIp[1], splitIp[2], splitIp[3], pasvPort >> 8, pasvPort & 0xff);
+        msg = temp;
+      }else{
+        msg = "530 Please login with USER.";
+      }
+      sendMsg(sock, msg);
+      printf("%d\n", PASV);
+      break;
+    case NLST:
+      if(flags.is_logged_in == 1){
+        if(flags.is_passive == 1){
+
+          
+        }else{
+          msg = "425 Use PASV first.";
+        }
+      }else{
+        msg = "530 Please login with USER.";
+      }
+      sendMsg(sock, msg);
+      printf("%d\n", NLST);
+      break;
+    case RETR:
+      printf("%d\n", RETR);
+      break;
+    case CWD:
+      printf("%d\n", CWD);
+      break;
+    case CDUP:
+      printf("%d\n", CDUP);
+      break;
+    case QUIT:
+      exit(0);
+    case INVALID:
+      msg = "500 - no this command\n";
+      sendMsg(sock, msg);
+    }
+
+    // if(strcasecmp(command,"EXIT")==0){
+    //   exit(0);
+    // }
+  }
+}
+
+command_t handeleCommand(char *s)
+{
+  if (strcasecmp(s, "USER") == 0)
+    return USER;
+
+  if (strcasecmp(s, "TYPE") == 0)
+    return TYPE;
+
+  if (strcasecmp(s, "MODE") == 0)
+    return MODE;
+
+  if (strcasecmp(s, "STRU") == 0)
+    return STRU;
+
+  if (strcasecmp(s, "PASV") == 0)
+    return PASV;
+
+  if (strcasecmp(s, "NLST") == 0)
+
+    return NLST;
+
+  if (strcasecmp(s, "RETR") == 0)
+
+    return RETR;
+
+  if (strcasecmp(s, "CWD") == 0)
+
+    return CWD;
+
+  if (strcasecmp(s, "CDUP") == 0)
+
+    return CDUP;
+
+  if (strcasecmp(s, "QUIT") == 0)
+
+    return QUIT;
+
+  return INVALID;
+}
+
+void splitStr(char *s, char delim, char *arr[])
+{
+  printf("str: %s\n", s);
+  printf("c: %c\n", delim);
+  char *p = strtok(s, &delim);
+  int i = 0;
+
+  while (p != NULL)
+  {
+    arr[i++] = p;
+    p = strtok(NULL, &delim);
+  }
+
+  for (i = 0; i < 4; ++i)
+    printf("%s\n", arr[i]);
+
+  return;
+}
+
+// void toUpper(char * s) {
+//   char * name;
+//   name = strtok(s,":");
+
+//   // Convert to upper case
+//   char *s = name;
+//   while (*s) {
+//     *s = toupper((unsigned char) *s);
+//     s++;
+//   }
+
+// }
